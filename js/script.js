238f9bb4f4915b8d9f1292af058710cb3bd87a1c
Rohan Chakravarty <rohan.chakravarty02@gmail.com>
2024-10-18 18:50:19 +0530

    Major UI Overhual

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
-function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
-}
 
 // Get the current year and set it in the footer
 document.getElementById('year').textContent = new Date().getFullYear();
+
+window.addEventListener('scroll', function() {
+  const menu = document.querySelector('.menu');
+  const scrollPosition = window.innerHeight + window.scrollY;
+  const pageHeight = document.documentElement.scrollHeight;
+
+  // Check if user has scrolled to the bottom
+  if (scrollPosition >= pageHeight) {
+      menu.classList.add('hidden'); // Add hidden class to hide the menu
+  } else {
+      menu.classList.remove('hidden'); // Remove hidden class to show the menu
+  }
+});
