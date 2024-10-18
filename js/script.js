82107239c838efc6fed14928e2f0b3f32a78c265
Rohan Chakravarty <rohan.chakravarty02@gmail.com>
2024-10-18 15:49:50 +0530

    Major Changes

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+// Get the current year and set it in the footer
+document.getElementById('year').textContent = new Date().getFullYear();
