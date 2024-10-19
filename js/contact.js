7967830655d994a7e11ddeedafc1bcabda735689
Rohan Chakravarty <rohan.chakravarty02@gmail.com>
2024-10-20 02:57:26 +0530

    added backend contact api

---
+++ js/contact.js
@@ -0,0 +1,22 @@
+async function submitForm(event) {
+    event.preventDefault();
+
+    const formData = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        contactNo: document.getElementById("contactNo").value,
+        message: document.getElementById("message").value,
+    };
+
+    const response = await fetch('http://localhost:8080/api/contact', { // Change to 8080 if that's where your Spring Boot app is running
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+    });
+    
+
+    const result = await response.text();
+    alert(result);
+}
