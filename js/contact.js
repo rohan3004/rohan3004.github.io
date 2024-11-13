e4bed9290d8d2de1993daf36dceb6ab9a740286a
Rohan Chakravarty <rohan.chakravarty02@gmail.com>
2024-11-13 11:05:28 +0530

    Fixed Minor Bugs

---
+++ js/contact.js
@@ -0,0 +1,22 @@
+async function submitForm(event) {
+    event.preventDefault();
+
+    const formData = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        contactNo: document.getElementById("contactNo").value,
+        message: document.getElementById("message").value,
+    };
+
+    const response = await fetch('https://api.rohandev.online/api/contact', { // Change to 8080 if that's where your Spring Boot app is running
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+    });
+    
+
+    const result = await response.text();
+    alert(result);
+}
