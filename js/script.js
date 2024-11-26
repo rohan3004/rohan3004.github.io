4a14d4096bbcec584136f2feb4500663bda45b2e
Rohan Chakravarty <rohan.chakravarty02@gmail.com>
2024-11-27 02:56:33 +0530

    Navbar Tweaks

--- js/script.js
+++ js/script.js
@@ -1,19 +1,6 @@
 // Get the current year and set it in the footer
 document.getElementById("year").textContent = new Date().getFullYear();
 
-window.addEventListener("scroll", function () {
-  const menu = document.querySelector(".menu");
-  const scrollPosition = window.innerHeight + window.scrollY;
-  const pageHeight = document.documentElement.scrollHeight;
-
-  // Check if user has scrolled to the bottom
-  if (scrollPosition >= pageHeight) {
-    menu.classList.add("hidden"); // Add hidden class to hide the menu
-  } else {
-    menu.classList.remove("hidden"); // Remove hidden class to show the menu
-  }
-});
-
 const nekosiaApiUrl = "https://api.nekosia.cat/api/v1/images/catgirl"; // Nekosia API for images
 const pexelsApiKey = "4mVQ8maC5iL6Q4TgKhuqy83X7dqKe2RMQHMEmNQkWCzHXEDq8twap8h1";
 
