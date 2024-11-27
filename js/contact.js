6f3e4b4e756e52ed095fd80789040a6f8b5b7e9c
Rohan Chakravarty <rohan.chakravarty02@gmail.com>
2024-11-27 19:50:00 +0530

    Bug Fix

--- js/contact.js
+++ js/contact.js
@@ -20,3 +20,17 @@ async function submitForm(event) {
   const result = await response.text();
   alert(result);
 }
+
+//Scroll Funtionality for hiding the menu when scrolled down
+window.addEventListener('scroll', function() {
+  const menu = document.querySelector('.menu');
+  const scrollPosition = window.innerHeight + window.scrollY;
+  const pageHeight = document.documentElement.scrollHeight;
+
+  // Check if user has scrolled to the bottom
+  if (scrollPosition >= pageHeight) {
+      menu.classList.add('hidden'); // Add hidden class to hide the menu
+  } else {
+      menu.classList.remove('hidden'); // Remove hidden class to show the menu
+  }
+});
