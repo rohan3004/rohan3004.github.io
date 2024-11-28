676ca649136cefc52fb599f11bde65f31157ad94
Rohan Chakravarty <rohan.chakravarty02@gmail.com>
2024-11-29 02:11:37 +0530

    Code Cleanup

--- js/contact.js
+++ js/contact.js
@@ -9,7 +9,6 @@ async function submitForm(event) {
   };
 
   const response = await fetch("https://api.rohandev.online/contact", {
-    // Change to 8080 if that's where your Spring Boot app is running
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -22,15 +21,15 @@ async function submitForm(event) {
 }
 
 //Scroll Funtionality for hiding the menu when scrolled down
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
   const menu = document.querySelector('.menu');
   const scrollPosition = window.innerHeight + window.scrollY;
   const pageHeight = document.documentElement.scrollHeight;
 
   // Check if user has scrolled to the bottom
   if (scrollPosition >= pageHeight) {
-      menu.classList.add('hidden'); // Add hidden class to hide the menu
+    menu.classList.add('hidden'); // Add hidden class to hide the menu
   } else {
-      menu.classList.remove('hidden'); // Remove hidden class to show the menu
+    menu.classList.remove('hidden'); // Remove hidden class to show the menu
   }
 });
--- js/script.js
+++ js/script.js
@@ -1,9 +1,6 @@
 // Get the current year and set it in the footer
 document.getElementById("year").textContent = new Date().getFullYear();
 
-const nekosiaApiUrl = "https://api.nekosia.cat/api/v1/images/catgirl"; // Nekosia API for images
-const pexelsApiKey = "4mVQ8maC5iL6Q4TgKhuqy83X7dqKe2RMQHMEmNQkWCzHXEDq8twap8h1";
-
 async function fetchGitHubData() {
   const username = "rohan3004";
 
@@ -29,7 +26,6 @@ async function fetchGitHubData() {
     if (Array.isArray(reposData)) {
       const repoContainer = document.getElementById("repo-container");
       for (const repo of filteredRepos) {
-        // const pexelsImageUrl = (await fetchPexelsImage(pexelsApiKey, repo.name)) || (await fetchNekosiaImage(nekosiaApiUrl));
 
         const hostedURL = `https://${username}.github.io/${repo.name}`;
 
@@ -270,36 +266,3 @@ fetch("https://api.rohandev.online/your_ip")
   .catch(error => {
     console.error("Error fetching IP from Spring Boot:", error);
   });
-
-
-// Fetch Nekosia Image for each repo
-// async function fetchNekosiaImage(apiUrl) {
-//   try {
-//     const response = await fetch(apiUrl);
-//     const data = await response.json();
-//     return data.image.original.url || null; // Return the original image URL from Nekosia API, or null if not available
-//   } catch (error) {
-//     console.error("Failed to fetch Nekosia image:", error);
-//     return null;
-//   }
-// }
-
-// Fetch Pexels Image for each repo
-// async function fetchPexelsImage(apiKey, repoName) {
-//   try {
-//     const randomPage = Math.floor(Math.random() * 10) + 1;
-//     const response = await fetch(
-//       `https://api.pexels.com/v1/search?query=${encodeURIComponent(
-//         repoName
-//       )}&per_page=1&page=${randomPage}`,
-//       {
-//         headers: { Authorization: apiKey },
-//       }
-//     );
-//     const data = await response.json();
-//     return data.photos[0]?.src?.medium || null; // Return medium-sized image URL, or null if not available
-//   } catch (error) {
-//     console.error("Failed to fetch Pexels image:", error);
-//     return null;
-//   }
-// }
